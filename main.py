5b13f61d5042e218e23241028426b04d79aa5612
JackyPrice <the.jacky.price@googlemail.com>
2021-06-20 16:50:45 +0100

    added 10 minute timer for website reload and check stock

--- main.py
+++ main.py
@@ -16,23 +16,26 @@ driver = webdriver.Chrome(options=opts)
 # driver.get("https://www.newegg.com/global/uk-en/p/pl?N=101582752%20600479300%2050001459%20600479299&cm_sp=Cat_Power-Supplies_3-_-VisNav-_-titanium-1200W-above-_3")
 
 # test on gpu in stock
-driver.get("https://www.newegg.com/global/uk-en/p/pl?N=101582767%20601331379%20601296707")
+while True:
+    driver.get("https://www.newegg.com/global/uk-en/p/pl?N=101582767%20601331379%20601296707")
 
-# wait for website to load
-time.sleep(5)
+    # wait for website to load
+    time.sleep(5)
 
-html = driver.page_source
-soup = bs4.BeautifulSoup(html, "html.parser")
-items_in_store = soup.find_all("div", {"class":"item-container"})
+    html = driver.page_source
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    items_in_store = soup.find_all("div", {"class":"item-container"})
 
-in_stock = []
-stock_count = 0
-for item in items_in_store:
-    if("OUT OF STOCK" not in item.text):
-        in_stock.append(item)
-        stock_count = stock_count + 1
+    in_stock = []
+    stock_count = 0
+    for item in items_in_store:
+        if("OUT OF STOCK" not in item.text):
+            in_stock.append(item)
+            stock_count = stock_count + 1
 
-print("after filter")
-print("there are " + str(stock_count) + " unique items in stock")
-for url in in_stock:
-    print(url)
+    print("after filter")
+    print("there are " + str(stock_count) + " unique items in stock")
+    for url in in_stock:
+        print(url)
+
+    time.sleep(600)
